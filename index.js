c38cc7b79841d002768ed4551b4c517a335c27ab
sebadev-img <sebastiandev.img@gmail.com>
2022-06-05 17:16:26 -0300

    version 1.0

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const tasks = require("./routes/tasks");
+const connectDB = require("./db/connect");
+require("dotenv").config();
+
+const app = express();
+const port = porcess.env.PORT || 5000;
+
+//middleware
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
+app.use("/api/v1/tasks", tasks);
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () => {
+      console.log(`server listening on port: ${port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
